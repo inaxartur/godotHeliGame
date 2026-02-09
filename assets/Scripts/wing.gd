c95549242656b583f06163473383ef86ad728788
Artur S <inaxit.artur@gmail.com>
2026-02-09 21:57:50 +0100

    Early implementation of induced velocites/flow, thrust

--- assets/Scripts/wing.gd
+++ assets/Scripts/wing.gd
@@ -14,24 +14,34 @@ var bladeLengthFeet := bladeLength * 3.2808
 @export var thrustCoefCurve : Curve
 var airDensity := 1.225
 
-var liftForce : float
-var heliPathVec : Vector3
-var heliSpeed : float = 0.1
+var thrust : float
+
+var liftForce : float = 0.0
+var heliPathVec : Vector3 = Vector3.ZERO
+var heliSpeed : float = 0.0
+var heliClimbRate : float = 0.0
 var bladeArea = bladeLength * bladeWidth # METERS^2
 var rotorArea = pow(bladeLength, 2) * PI
 var solidity = (bladeArea * 4) / rotorArea
 var advanceRatio : float = 0.0
+var KTAS : float
+
+var inducedVelocity : float
+var inflowVelocity : float
+var netDownFlowSpeed : float
+var inflowRatio : float
 
 var yawAxis : Vector3 = Vector3.ZERO
 var pitchAxis : Vector3 = Vector3.ZERO
 var rollAxis : Vector3 = Vector3.ZERO
 
 var globalRollAxis : Vector3 = Vector3.BACK
-var heliPitchDeg : float
+var heliPitch : float
 
 var bladePitch : float
 var aoa : float
 var liftCoef : float
+var thrustCoef : float
 var rpm : float
 var bladeSpeed : float
 var debugIter := 0
@@ -46,16 +56,24 @@ func _process(delta: float) -> void:
 	pitchAxis = global_transform.basis.x
 	rollAxis = global_transform.basis.z
 	
-	heliPitchDeg = heliNode.heliPitchDeg
+	heliPitch = heliNode.heliPitch
 	heliPathVec = heliNode.pathVector
+	heliClimbRate = heliNode.rateOfClimb.length()
 	heliSpeed = heliPathVec.length() / delta
 	rpm = heliNode.rpm
-	bladeSpeed = (rpm * PIdivby30 * 3.0) + heliSpeed / 50
 	
-	advanceRatio = (bladeSpeed*50 * cos(heliPitchDeg)) / (rpm * bladeLengthFeet)
+	bladeSpeed = (rpm * PIdivby30 * bladeLength)
+	KTAS = heliSpeed * 16
 	
+	advanceRatio = (KTAS * cos(heliPitch)) / (rpm * bladeLengthFeet) ## ADVANCE RATIO CALC
+	thrustCoef = thrustCoefCurve.sample(advanceRatio) ## THRUST COEFFICIENT
+	thrust = thrustCoef * airDensity * rotorArea * pow(rpm * bladeLength, 2)
 	
-	
+	inducedVelocity = sqrt(thrust / (2 * airDensity * rotorArea))
+	#1inducedVelocity = (-(heliClimbRate/2) + sqrt(pow(heliClimbRate/2, 2) + (thrust / (2 * airDensity * rotorArea))))
+	inflowVelocity = inducedVelocity + heliClimbRate
+	netDownFlowSpeed = (KTAS * sin(heliPitch)) + (heliClimbRate * cos(heliPitch)) + inducedVelocity
+	inflowRatio = netDownFlowSpeed / rpm * bladeLength
 	
 	bladePitch = 0.06 - (heliNode.collective / 300) # RADIANS
 	aoa = 0 - (heliNode.collective / 300) # RADIANS
@@ -71,9 +89,14 @@ func drawVectors() -> void:
 		print("aoa: ", aoa)
 		print("rpm: ", rpm)
 		print("bladeSpeed: ", bladeSpeed)
-		print("liftCoef: ", liftCoef)
-		print("liftForce: ", liftForce)
+		#print("liftCoef: ", liftCoef)
+		#print("liftForce: ", liftForce)
+		print("thrustCoef: ", thrustCoef)
+		print("thrust: ", thrust)
 		print("Advance ratio: ", advanceRatio)
+		print("induced velocity: ", inducedVelocity)
+		print("net down flow speed: ", netDownFlowSpeed)
+		print("inflow ratio: ", inflowRatio)
 		#print("solidity: ", solidity)
 		debugIter = 0
 	else:
